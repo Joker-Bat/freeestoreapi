8ce836a7e8550724baae329fdb908fadd1a3d4c3
Joker-Bat <shanmugam091098@gmail.com>
2021-06-19 18:55:19 +0530

    Added Database details

---
+++ models/productModel.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const productSchema = new mongoose.Schema({
+  ref: {
+    type: Number,
+    required: [true, 'Product must have a reference number.'],
+  },
+  id: {
+    type: String,
+    required: [true, 'Product must have a id.'],
+  },
+  type: {
+    type: String,
+    required: [true, 'Product must have a type.'],
+  },
+  brand: {
+    type: String,
+    required: [true, 'Product must have a brand'],
+  },
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, 'Product must have a name.'],
+  },
+  slug: String,
+  price: {
+    type: Number,
+    required: [true, 'Product must have a price.'],
+  },
+  realPrice: {
+    type: Number,
+    required: [true, 'Product must have a realPrice.'],
+  },
+  bannerImage: String,
+  images: {
+    type: [String],
+    required: [true, 'Product must have atlease one image.'],
+  },
+  specs: {
+    processor: String,
+    diskType: String,
+    diskSize: String,
+    ram: String,
+  },
+  highlights: {
+    type: [String],
+    required: [true, 'Product must have a highlights'],
+  },
+  ratings: [
+    {
+      stars: {
+        type: Number,
+        required: [true, 'Rating must have a star'],
+        min: [1, 'Rating Average should above 1'],
+        max: [5, 'Ratings Average should below 5'],
+        set: (val) => Math.round(val * 10) / 10,
+      },
+      title: {
+        type: String,
+        required: [true, 'Rating must have a title'],
+      },
+      description: {
+        type: String,
+        required: [true, 'Rating must have a description'],
+      },
+      userName: {
+        type: String,
+        required: [true, 'Rating must have a userName'],
+      },
+      daysBefore: {
+        type: String,
+        required: [true, 'Rating must have a daysBefore'],
+      },
+    },
+  ],
+});
+
+productSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 // 3rd party modules
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+// DB Connection Details (URL + Password)
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+// DB Connection
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then('Connected To DB Successfully! 👲')
+  .catch((err) => console.log('Error in DB Connection 🎗️', err));
+
+// Watch for Errors in connection after initial establish
+mongoose.connection.on('error', (err) => {
+  console.log('Error in DB Connection 🚨', err);
+});
+
+// Run server
 const port = process.env.PORT || 8000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
